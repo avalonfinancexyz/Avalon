6106d80f1fa5e828c670d2714e2cce4f811b154d
4v410n <is4v410n@gmail.com>
2024-06-07 22:01:47 +0800

    chore: add cowLP & macLP

--- markets/bitlayer/index.ts
+++ markets/bitlayer/index.ts
@@ -13,6 +13,8 @@ import {
   strategyBITUSD,
   strategySTBTC,
   strategySOLVBTC,
+  strategySTBTCWBTCCOWLP,
+  strategySTBTCWBTCMACLP,
 } from "./reservesConfigs";
 import {
   rateStrategyVolatileOne,
@@ -44,6 +46,8 @@ export const BitlayerConfig: IAaveConfiguration = {
     BITUSD: strategyBITUSD,
     STBTC: strategySTBTC,
     SOLVBTC: strategySOLVBTC,
+    STBTCWBTCCOWLP: strategySTBTCWBTCCOWLP,
+    STBTCWBTCMACLP: strategySTBTCWBTCMACLP,
   },
   ReserveAssets: {
     [eBitlayerNetwork.main]: {
@@ -54,6 +58,8 @@ export const BitlayerConfig: IAaveConfiguration = {
       BITUSD: "0x07373d112EDc4570B46996Ad1187bc4ac9Fb5Ed0",
       STBTC: "0xf6718b2701d4a6498ef77d7c152b2137ab28b8a3",
       SOLVBTC: "0xe04d21d999faedf1e72ade6629e20a11a1ed14fa",
+      STBTCWBTCCOWLP: "0xb88a54ebbda8edbc1c2816ace1dc2b7c6715972d",
+      STBTCWBTCMACLP: "0xb750f79cf4768597f4d05d8009fcc7cee2704824",
     },
     [eBitlayerNetwork.testnet]: {
       WBTC: "0xfF204e2681A6fA0e2C3FaDe68a1B28fb90E4Fc5F",
@@ -100,6 +106,8 @@ export const BitlayerConfig: IAaveConfiguration = {
       BITUSD: "0xD62d417Ccf5A7CF8022aF7CaE7F736d4069e9aaC",
       STBTC: "0x29304F0c6fd0Db604312d6E982164e1E7C3adAD9",
       SOLVBTC: "0x29304F0c6fd0Db604312d6E982164e1E7C3adAD9",
+      STBTCWBTCCOWLP: "0xcea32dC4e973a77e995bA2412623a09892ea9751",
+      STBTCWBTCMACLP: "0x29304F0c6fd0Db604312d6E982164e1E7C3adAD9",
     },
     [eBitlayerNetwork.testnet]: {
       WBTC: "0x2b3f685266524e921cb5dd3094e57e85a3000487",
--- markets/bitlayer/reservesConfigs.ts
+++ markets/bitlayer/reservesConfigs.ts
@@ -133,3 +133,39 @@ export const strategySOLVBTC: IReserveParams = {
   debtCeiling: "0",
   borrowableIsolation: false,
 };
+
+export const strategySTBTCWBTCCOWLP: IReserveParams = {
+  strategy: rateStrategyVolatileBTC,
+  baseLTVAsCollateral: "0",
+  liquidationThreshold: "0",
+  liquidationBonus: "0",
+  liquidationProtocolFee: "1000",
+  borrowingEnabled: false,
+  stableBorrowRateEnabled: false,
+  flashLoanEnabled: false,
+  reserveDecimals: "18",
+  aTokenImpl: eContractid.AToken,
+  reserveFactor: "3000",
+  supplyCap: "0",
+  borrowCap: "0",
+  debtCeiling: "0",
+  borrowableIsolation: false,
+};
+
+export const strategySTBTCWBTCMACLP: IReserveParams = {
+  strategy: rateStrategyVolatileBTC,
+  baseLTVAsCollateral: "0",
+  liquidationThreshold: "0",
+  liquidationBonus: "0",
+  liquidationProtocolFee: "1000",
+  borrowingEnabled: false,
+  stableBorrowRateEnabled: false,
+  flashLoanEnabled: false,
+  reserveDecimals: "18",
+  aTokenImpl: eContractid.AToken,
+  reserveFactor: "3000",
+  supplyCap: "0",
+  borrowCap: "0",
+  debtCeiling: "0",
+  borrowableIsolation: false,
+};
