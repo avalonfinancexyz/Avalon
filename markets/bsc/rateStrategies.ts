62619f5bca9fe600e0f19aa2c8c93b8daf75d591
4v410n <is4v410n@gmail.com>
2024-06-08 15:44:24 +0800

    config: update rate strategy for bsc

--- markets/bsc/rateStrategies.ts
+++ markets/bsc/rateStrategies.ts
@@ -83,7 +83,7 @@ export const rateStrategyVolatileBTCOne: IInterestRateStrategyParams = {
   name: "rateStrategyVolatileBTCOne",
   optimalUsageRatio: parseUnits("0.75", 27).toString(),
   baseVariableBorrowRate: parseUnits("0", 27).toString(),
-  variableRateSlope1: parseUnits("0.05", 27).toString(),
+  variableRateSlope1: parseUnits("0.10", 27).toString(),
   variableRateSlope2: parseUnits("0.5", 27).toString(),
   stableRateSlope1: parseUnits("0.07", 27).toString(),
   stableRateSlope2: parseUnits("3", 27).toString(),
