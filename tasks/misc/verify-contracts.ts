50f4c8d8c69ac87e96ad7ae0ead2864678e3ce35
4v410n <is4v410n@gmail.com>
2024-09-12 16:28:06 +0800

    chore: add verify task

---
+++ tasks/misc/verify-contracts.ts
@@ -0,0 +1,12 @@
+import { task } from "hardhat/config";
+import { eNetwork } from "../../helpers";
+
+task(`verify-contracts`).setAction(
+  async (_, { deployments, getNamedAccounts, ...hre }) => {
+    await hre.run("verify-core");
+    await hre.run("verify-market");
+    await hre.run("verify-periphery-post");
+    await hre.run("verify-periphery");
+    await hre.run("verify-tokens");
+  }
+);
